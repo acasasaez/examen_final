18315921eb1a1c8644069a30987c5192cdcce29a
acasasaez <acasasae@myuax.com>
2022-05-12 09:18:01 +0200

    limpieza de datasets

--- examen.py
+++ examen.py
@@ -6,6 +6,7 @@ from pandas import panda as pd
 df_navegaciones = pd.read_csv(r"navegaciones.csv", sep = ";")
 df_conversiones = pd.read_cv(r"conversiones.csv", sep=";")
 
+#Limpiamos los datasets 
 df_navegaciones_limpio = df_navegaciones.dropna()
 df_conversiones_limpio = df_conversiones.dropna()
 
