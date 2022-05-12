24d885cde9950a86d89c7b81e15b3be47f4b6c3f
acasasaez <acasasae@myuax.com>
2022-05-12 09:11:39 +0200

    limpieza de datatasets

--- examen.py
+++ examen.py
@@ -5,3 +5,7 @@ from pandas import panda as pd
 #Lectura de archivos csv con panda 
 df_navegaciones = pd.read_csv(r"navegaciones.csv", sep = ";")
 df_conversiones = pd.read_cv(r"conversiones.csv", sep=";")
+
+df_navegaciones_limpio = df_navegaciones.dropna()
+df_conversiones_limpio = df_conversiones.dropna()
+
